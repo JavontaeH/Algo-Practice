872f0df42047753d809756280bd3b125068f6a3f
JavontaeH <hardeman.javontae@gmail.com>
2022-09-06 16:01:56 -0500

    single number

---
+++ SingleNumber/singnum.js
@@ -0,0 +1,19 @@
+// Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+
+var singleNumber = function (nums) {
+  let ans = 0;
+  for (var i = 0; i < nums.length; i++) {
+    let count = 0;
+    for (var j = 0; j < nums.length; j++) {
+      if (nums[i] === nums[j]) {
+        count++;
+      }
+    }
+    if (count === 1) {
+      ans = nums[i];
+    }
+  }
+  return ans;
+};
+
+console.log(singleNumber([2, 2, 1]));
