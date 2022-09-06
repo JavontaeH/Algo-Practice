8f29c7c420993827c68f4dcddb887f5ff43637c1
JavontaeH <hardeman.javontae@gmail.com>
2022-09-06 15:55:20 -0500

    finished btit

--- BinaryTreeInorderTraversal/btit.js
+++ BinaryTreeInorderTraversal/btit.js
@@ -1,3 +1,25 @@
 // Given the root of a binary tree, return the inorder traversal of its nodes' values.
 
-var inorderTraversal = function (root) {};
+function inorderTraversal(root) {
+  if (!root) {
+    return [];
+  }
+
+  const result = [];
+  const nodesToTraverse = [];
+
+  let currentNode = root;
+
+  while (nodesToTraverse.length || currentNode) {
+    while (currentNode) {
+      nodesToTraverse.push(currentNode);
+      currentNode = currentNode.left;
+    }
+
+    currentNode = nodesToTraverse.pop();
+    result.push(currentNode.val);
+    currentNode = currentNode.right;
+  }
+
+  return result;
+}
