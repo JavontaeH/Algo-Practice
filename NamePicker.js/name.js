650c8e40c2eacd84084af164ecfa3fefa05f7bbe
JavontaeH <hardeman.javontae@gmail.com>
2022-10-05 19:34:33 -0500

    finished

--- NamePicker.js/name.js
+++ NamePicker.js/name.js
@@ -1,6 +1,21 @@
 function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
+// function that returns the first occurance of the array generated twice.
+const picker = (arr) => {
+  let chosen = [];
+  let picked = false;
+  while (picked !== true) {
+    let name = arr[getRandomInt(arr.length - 1)];
+    if (chosen.find((char) => char === name) !== undefined) {
+      picked === true;
+      return name;
+    } else {
+      chosen.push(name);
+    }
+  }
+};
+
 let names = [
   "nago",
   "ky",
@@ -16,18 +31,4 @@ let names = [
   "gold",
   "brisket",
 ];
-const picker = (arr) => {
-  let chosen = [];
-  let picked = false;
-  while (picked !== true) {
-    let name = arr[getRandomInt(arr.length - 1)];
-    if (chosen.find((char) => char === name) !== undefined) {
-      picked === true;
-      return name;
-    } else {
-      chosen.push(name);
-    }
-  }
-};
-
 console.log(picker(names));
